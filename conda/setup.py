bb3ccb12c25dba8ad2652369840e46cdd648079a
Hans Ghezzi <65049127+hghezzi@users.noreply.github.com>
2024-04-23 17:39:41 -0400

    Add files via upload

---
+++ conda/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='puppy',
+    use_scm_version={
+            "local_scheme": "node-and-timestamp",
+            "fallback_version": "1.1.1",
+        },
+    packages=find_packages(),
+    install_requires=[
+        'primer3-py>=0.6.1',
+        'seaborn>=0.13.0',
+        'colorama>=0.4.1'
+    ],
+    setup_requires=['setuptools_scm'],
+    entry_points={
+        'console_scripts': [
+            'puppy-align=puppy.puppy_align:main',
+            'puppy-primers=puppy.puppy_primers:main',
+            'puppy-GUI=puppy.puppy_GUI:main'
+        ]
+    }
+)
